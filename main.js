99a746b57ce4e108c784462c4ed640a625372dd5
Runeviperz <battlefield200@gmail.com>
2017-02-27 17:50:51 +1100

    working prototype

--- main.js
+++ main.js
@@ -4,13 +4,36 @@ function doRoll() {
     $("#roll-id").text(random);
 }
 
+function calculateWinnings(roll) {
+    var distance = Math.abs(50-roll);
+    var winningsMult = Math.floor(Math.exp((20-distance)/10));
+    return parseInt($("#bet-amount").text(), 10) * winningsMult;
+}
+
+function updateTotal(winnings) {
+    
+    var betAmount = parseInt($("#bet-amount").text(), 10);
+    var total = parseInt($("#total-amount").text(), 10);
+    var newTotal = total-betAmount+winnings;
+    $("#total-amount").text(newTotal.toString());
+}
+
 function beginning() {
   
     $(".main").hide();
     $(".main").fadeIn(1000);
     $(".spin-button").on("click", function() {
         
-        doRoll();
+        if (parseInt($("#bet-amount").text(), 10) <= parseInt($("#total-amount").text(), 10)) {
+            doRoll(); 
+            var winnings = calculateWinnings(parseInt($("#roll-id").text(), 10));
+            updateTotal(winnings);                                 
+        } else {
+            
+            
+        }
+        
+        
     });
 }
 
